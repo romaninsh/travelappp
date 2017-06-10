2635295d17c54303eb4cca46c39ca3c9289046b8
Alex00Sam <alsa00@mail.ru>
2017-06-10 14:03:53 +0300

    test

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 	$form=$app->layout->add('Form');
 	$form->setModel(new client($db));
 	$form->onSubmit(function($form) {
+		if ($form->'name'<>"") and ($form->'surname'<>"") and ($form->'email'<>"") and ($form->'vacation'<>"") {
 		$form->model->save();
 		return $form->success('Have a great vacation, '.$form->model['name'].' !!!');
+		}
+		else return $form->error('You must fill all the fields!');
 			
 	});
