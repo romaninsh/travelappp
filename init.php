d479bbfd85cabc6f8fe357d5e4facc3eb331a38f
Romans Malinovskis <me@nearly.guru>
2017-06-13 18:47:25 +0000

    Apply fixes from StyleCI

--- init.php
+++ init.php
@@ -1,8 +1,9 @@
 <?php
+
 require 'vendor/autoload.php';
 
 //New app//
-$app=new \atk4\ui\App('Travelappp');
+$app = new \atk4\ui\App('Travelappp');
 ///////////
 
 //Layout//
@@ -10,5 +11,5 @@ $layout = $app->initLayout('Centered');
 //////////
 
 //DB Connection//
-$db = new 
-\atk4\data\Persistence_SQL('mysql:dbname=heroku_280a158781dc374;host=eu-cdbr-west-01.cleardb.com','b43963c1dcd91f','8ed0930d');
+$db = new
+\atk4\data\Persistence_SQL('mysql:dbname=heroku_280a158781dc374;host=eu-cdbr-west-01.cleardb.com', 'b43963c1dcd91f', '8ed0930d');
--- reg.php
+++ reg.php
@@ -1,29 +1,29 @@
 <?php
-	require 'init.php';
-	$form=$app->layout->add('Form');
-	$form->setModel(new client($db));
-	$form->onSubmit(function($form) {
-//		if ($form->client['name']<>NULL and $form->client['surname']<>NULL and $form->client['email']<>NULL and $form->client['destination']<>NULL) {
-			$form->model->save();
-			return $form->success('Have a great vacation, '.$form->model['name'].' !!!');
+    require 'init.php';
+    $form = $app->layout->add('Form');
+    $form->setModel(new client($db));
+    $form->onSubmit(function ($form) {
+        //		if ($form->client['name']<>NULL and $form->client['surname']<>NULL and $form->client['email']<>NULL and $form->client['destination']<>NULL) {
+            $form->model->save();
+
+        return $form->success('Have a great vacation, '.$form->model['name'].' !!!');
 /*		}
-		else {
-			if ($form->client['name']==NULL) {
-				return $form->error('name',"You must fill this field!");
-			}
-			if ($form->client['surname']==NULL) {
-				return $form->error('surname',"You must fill this field!");
-			}
-			if ($form->client['email']==NULL) {
-				return $form->error('email',"You must fill this field!");
-			}	
-			if ($form->client['destination']==NULL) {
-				return $form->error('destination',"You must fill this field!");
-			}
-		}  */
-		
-	});
-	
-	$back=$app->layout->add('Button');
-	$back->set('Back');
-	$back->link('index.php');
+        else {
+            if ($form->client['name']==NULL) {
+                return $form->error('name',"You must fill this field!");
+            }
+            if ($form->client['surname']==NULL) {
+                return $form->error('surname',"You must fill this field!");
+            }
+            if ($form->client['email']==NULL) {
+                return $form->error('email',"You must fill this field!");
+            }
+            if ($form->client['destination']==NULL) {
+                return $form->error('destination',"You must fill this field!");
+            }
+        }  */
+    });
+
+    $back = $app->layout->add('Button');
+    $back->set('Back');
+    $back->link('index.php');
